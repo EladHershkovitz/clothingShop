712413fc33a1eff91fecde0c13e8e8b5e6cf1f1c
eladher <eladhershkoviz@gmail.com>
2020-12-31 15:53:40 -0600

    feat: add ability to store authenticated users into db (firesotre)

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -25,9 +25,9 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <FormInput name="email" type="email" label="email" value={this.state.email} handleChange={this.handleChange} required />
+          <FormInput name="email" type="email" label="Email" value={this.state.email} handleChange={this.handleChange} required />
 
-          <FormInput name="password" type="password" label="password" value={this.state.password} handleChange={this.handleChange} required />
+          <FormInput name="password" type="password" label="Password" value={this.state.password} handleChange={this.handleChange} required />
           <div className="buttons">
             <CustomButton type="submit">Sign in</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
